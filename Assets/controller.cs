9a33983bf9fb12b36b16c1e2b1ab39920462da29
Yasin Açıkgöz <yasinacikgoz@Yasin-MacBook-Pro.local>
2018-01-08 22:58:20 +0300

    controller.cs comments added

--- Assets/controller.cs
+++ Assets/controller.cs
@@ -6,25 +6,24 @@ using System;
 public class controller : MonoBehaviour
 {
 
-    //public GameObject ground;
+
     private bool walking = true;
     private Vector3 spanPoint;
 
-    // Use this for initialization
     void Start()
     {
         spanPoint = transform.position;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         if (walking)
             transform.position = transform.position + Camera.main.transform.forward * .5f * Time.deltaTime;
         
 
+        // oyunu yeniden baslat
         if (transform.position.y < -10f)
-        
             transform.position = spanPoint;
         
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
@@ -32,7 +31,7 @@ public class controller : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
 		{
-			
+			//asagiya bakinca veya duvara gelince dur
 			if (hit.collider.name.Contains("plane") || hit.collider.name.Contains("cube") )
             {
                 walking = false;
